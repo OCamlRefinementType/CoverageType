931a2553521fdb2b5bcfd5131357284dd9f95b19
Kevin Yu <kevinyu567@gmail.com>
2026-04-07 18:40:26 -0400

    Add rbleaf2

---
+++ data/bench/subtyping/rbleaf2/axioms_post.ml
@@ -0,0 +1,3 @@
+let[@axiom] rbtree_rb_leaf_no_rb_root_color = fun (l : (int rbtree)) (x : bool) -> ((rb_leaf l) #==> (not (rb_root_color l x)))
+let[@axiom] rbtree_rb_leaf_no_red_red = fun (l : (int rbtree)) -> ((rb_leaf l) #==> (no_red_red l))
+let[@axiom] rbtree_rb_leaf_num_black_0_second = fun (l : (int rbtree)) -> ((rb_leaf l) #==> (num_black l 0))
